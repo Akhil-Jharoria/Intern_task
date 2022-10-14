30a8d954c82d74d455aeea379363cba3bf2b4e91
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2022-10-14 12:10:47 +0530

    Create aboutus.php

---
+++ payment_gateway/aboutus.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--
+About Us Page
+-->
+<html>
+
+<head>
+    <title>About Us</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+
+    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"/> -->
+    <link rel="stylesheet" href="style/style1.css" type="text/css">
+    <!-- <link rel="stylesheet" href="style.css" type="text/css"/> -->
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">SPARK NGO</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> HOME</a></li>
+                        <li><a href="aboutus.php"><span class="glyphicon glyphicon-book"></span>ABOUT US</a></li>
+                        <li><a href="contactform.php"><span class="glyphicon glyphicon-phone"></span>CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+
+    <section class="about_area section_gap">
+        <div class="container">
+            <div class="row">
+                <div class="single_about row">
+                    <div class="col-lg-6 col-md-12 text-center about_left">
+                        <div class="about_thumb">
+                            <img src="" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 about_right">
+                        <div class="about_content">
+                            <h2>"Not all of us can do great things.But we can do small things with great love.❣️"</h2>
+                            <h2>
+                                We are nonprofit team <br>
+                                and work worldwide
+                            </h2>
+                            <p>
+                                Our approach is rooted in years of on-the-ground experience,
+                                driven by a careful methodology and powered by a deep passion
+                                to improve the lives of children and give them a chance to grow
+                                up in a family.
+                            </p>
+                            <p>
+                                We believe in collaboration as we know this work is bigger than
+                                any one organization. We are part of a global network of nonprofit
+                                organizations leading the worldwide movement.
+                            </p>
+                            <a href="#" class="primary_btn">Learn more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <center>
+                <p>Copyright &copy; Spark NGO. All Rights Reserved | Contact Us: +91 111111 111111</p>
+            </center>
+        </div>
+    </footer>
+</body>
+</html>
